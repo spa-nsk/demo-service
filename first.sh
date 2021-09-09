42ff337ee520f1206524829ea9441f024d097b8a
spa-nsk <pavel_semenov@mail.ru>
2021-09-09 22:08:41 +0700

    обновление

--- first.sh
+++
@@ -1,7 +0,0 @@
-echo "# demo-service" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/spa-nsk/demo-service.git
-git push -u origin main
--- next.sh
+++
@@ -1,4 +0,0 @@
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/spa-nsk/demo-service.git
-git push -u origin main
